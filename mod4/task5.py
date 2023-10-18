aa7cc0699eaad2fd96317de471deabc390157f15
ToyoTaSX <toyotas_2004@mail.ru>
2023-10-18 21:37:20 +0500

    update

--- mod4/task5.py
+++ mod4/task5.py
@@ -8,10 +8,19 @@ def get_counts(s: str) -> dict:
                 result[ch] = 1
     return result
 
+
 def get_file_statistics(filename: str) -> dict:
     s = ''
     with open(filename, 'r', encoding='utf-8') as file:
         s = ' '.join(file.readlines())
-    return dict(sorted(get_counts(s).items(), key= lambda item: item[1]))
+    return dict(sorted(get_counts(s).items(), key=lambda item: item[1]))
 
-print(get_file_statistics("test.txt"))
+
+def write_dict_to_file(filename: str, dictionary: dict):
+    with open(filename, 'w', encoding='utf-8') as file:
+        for key, value in dictionary.items():
+            file.write(f"{str(key)}: {str(value)}\n")
+
+
+stats = get_file_statistics("test.txt")
+write_dict_to_file("out.txt", stats)
